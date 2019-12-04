53ded87c92b4b8e00f554a51c95db23aff1d1720
Nithin Shenoy <nshenoy@mimeo.com>
2019-12-04 08:42:44 -0500

    Adding initial code

---
+++ lambda/py/alexa/data.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+
+# Resolving gettext as _ for module loading.
+from gettext import gettext as _
+
+SKILL_NAME = "Locust Valley Lunch Menu"
+
+WELCOME = _("Welcome to the Locust Valley Lunch Menu")
+HELP = _("Say about, to hear more about the skill, or what's for lunch at AMP or Middle School today or tomorrow ")
+ABOUT = _("I'll tell you what's for lunch today or tomorrow at AMP or Middle School")
+STOP = _("Okay, see you next time!")
+FALLBACK = _("The {} can't help you with that.")
+GENERIC_REPROMPT = _("What can I help you with?")
+LUNCHMENU = "December2019"
+LUNCHMENU_DATA_AMP = [
+    {
+        "date": "2019-12-02",
+        "item": "Mozzarella Sticks with Marinara Sauce"
+    },
+    {
+        "date": "2019-12-03",
+        "item": "Turkey Nachos Grande"
+    },
+    {
+        "date": "2019-12-04",
+        "item": "Pasta with Meat Sauce"
+    },
+    {
+        "date": "2019-12-05",
+        "item": "Chicken Nuggets"
+    },
+    {
+        "date": "2019-12-06",
+        "item": "Pizza Cheese Crunchers"
+    },
+    {
+        "date": "2019-12-09",
+        "item": "Maple Glazed French Toast Sticks"
+    },
+    {
+        "date": "2019-12-10",
+        "item": "Grilled Cheese Sandwich"
+    },
+    {
+        "date": "2019-12-11",
+        "item": "Hamburger or Cheeseburger or Veggie Burger on a Bun"
+    },
+    {
+        "date": "2019-12-12",
+        "item": "Chicken Tenders"
+    },
+    {
+        "date": "2019-12-13",
+        "item": "Pizza Slice"
+    },
+    {
+        "date": "2019-12-16",
+        "item": "Egg & Cheese Sandwich on a Bun"
+    },
+    {
+        "date": "2019-12-17",
+        "item": "Meatball Hero Parmesan"
+    },
+    {
+        "date": "2019-12-18",
+        "item": "Mozzarella Sticks with Marinara Sauce"
+    },
+    {
+        "date": "2019-12-19",
+        "item": "Popcorn Chicken"
+    },
+    {
+        "date": "2019-12-20",
+        "item": "Pizza Sticks"
+    }
+]
+LUNCHMENU_DATA_MIDDLESCHOOL = []
---
+++ lambda/py/alexa/util.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+import requests
+from ask_sdk_model import IntentRequest
+from typing import Union, Dict, List
+
+def get_lunch(lunch_data, searchDate):
+    for lunchItem in lunch_data:
+        if lunchItem["date"] == searchDate:
+            return lunchItem
+
+    if calcDate.weekday() < 5:
+        return {"item": "NOTHING because it is a holiday!"}
+    else:
+        return {"item": "NOTHING because it is the weekend!"}        
+
+def get_lunch_for_today(lunch_data):
+    calcDate = datetime.today()
+    return get_lunch(lunchItem, calcDate)
+
+def get_lunch_for_tomorrow(lunch_data):
+    calcDate = (datetime.today() + timedelta(days=1))
+    return get_lunch(lunchItem, calcDate)
+
+def get_resolved_value(request, slot_name):
+    """Resolve the slot name from the request."""
+    # type: (IntentRequest, str) -> Union[str, None]
+    try:
+        return request.intent.slots[slot_name].value
+    except (AttributeError, ValueError, KeyError, IndexError):
+        return None
