9bf3a51d1506278e5aec1f738f50512ac6cf8e26
nshenoy <nithin_shenoy@hotmail.com>
2020-02-03 07:54:29 -0500

    Adds Februrary menu

--- lambda/py/alexa/data.py
+++ lambda/py/alexa/data.py
@@ -11,176 +11,130 @@ ABOUT = _("I'll tell you what's for lunch today or tomorrow in Locust Valley Ele
 STOP = _("Okay, see you next time!")
 FALLBACK = _("The {} can't help you with that.")
 GENERIC_REPROMPT = _("What can I help you with?")
-LUNCHMENU = "December2019"
+LUNCHMENU = "February2020"
 LUNCHMENU_DATA_ELEMENTARY = [
-    {
-        "date": "2020-01-02",
-        "item": "Fish Nuggets"
-    },
-    {
-        "date": "2020-01-03",
-        "item": "Pizza Slice"
-    },
-    {
-        "date": "2020-01-06",
-        "item": "Grilled Cheese Sandwich"
-    },
-    {
-        "date": "2020-01-07",
-        "item": "Turkey Nachos Grande"
-    },
-    {
-        "date": "2020-01-08",
-        "item": "Meatball Hero Parmesan"
-    },
-    {
-        "date": "2020-01-09",
-        "item": "Chicken Nuggets"
-    },
-    {
-        "date": "2020-01-10",
-        "item": "Pizza Cheese Crunchers"
-    },
-    {
-        "date": "2020-01-13",
-        "item": "Maple Glazed French Toast Sticks"
-    },
-    {
-        "date": "2020-01-14",
-        "item": "Hot Dog on Bun"
-    },
-    {
-        "date": "2020-01-15",
-        "item": "Pasta with Meat Sauce"
-    },
-    {
-        "date": "2020-01-16",
-        "item": "Chicken Tenders"
-    },
-    {
-        "date": "2020-01-17",
-        "item": "Pizza Slice"
-    },
-    {
-        "date": "2020-01-21",
-        "item": "Homemade Macaroni and Cheese"
-    },
-    {
-        "date": "2020-01-22",
-        "item": "Hamburger or Cheeseburger or Veggie Burger on a Bun"
-    },
-    {
-        "date": "2020-01-23",
-        "item": "Popcorn Chicken"
-    },
-    {
-        "date": "2020-01-24",
-        "item": "Pizza Sticks"
-    },
-    {
-        "date": "2020-01-27",
-        "item": "Egg and Cheese on a Bun"
-    },
-    {
-        "date": "2020-01-28",
-        "item": "Twin Turkey Tacos"
-    },
-    {
-        "date": "2020-01-29",
-        "item": "Mozzarella Sticks with Marinara Sauce"
-    },
-    {
-        "date": "2020-01-30",
-        "item": "Chicken Nuggets"
-    },
-    {
-        "date": "2020-01-31",
-        "item": "Pizza Slice"
-    }
+    [
+        {
+            "date": "2020-02-03",
+            "item": "Grilled Cheese Sandwich"
+        },
+        {
+            "date": "2020-02-04",
+            "item": "Meatball Hero Parmesan"
+        },
+        {
+            "date": "2020-02-05",
+            "item": "Mozzarella Sticks with Marinara Sauce"
+        },
+        {
+            "date": "2020-02-06",
+            "item": "Chicken Tenders"
+        },
+        {
+            "date": "2020-02-07",
+            "item": "Pizza Cheese Crunchers"
+        },
+        {
+            "date": "2020-02-10",
+            "item": "Maple Glazed French Toast Sticks"
+        },
+        {
+            "date": "2020-02-11",
+            "item": "Homemade Macaroni and Cheese"
+        },
+        {
+            "date": "2020-02-12",
+            "item": "Hamburger or Cheeseburger or Veggie Burger on a Bun"
+        },
+        {
+            "date": "2020-02-13",
+            "item": "Chicken Nuggets"
+        },
+        {
+            "date": "2020-02-14",
+            "item": "Pizza Sticks"
+        },
+        {
+            "date": "2020-02-24",
+            "item": "Crispy Chicken Patty on a Bun"
+        },
+        {
+            "date": "2020-02-25",
+            "item": "Hot Dog on Bun"
+        },
+        {
+            "date": "2020-02-26",
+            "item": "Pasta with Marinara Sauce"
+        },
+        {
+            "date": "2020-02-27",
+            "item": "Popcorn Chicken"
+        },
+        {
+            "date": "2020-02-28",
+            "item": "Pizza Slice"
+        }
+    ]
 ]
 LUNCHMENU_DATA_MIDDLESCHOOL = [
     {
-        "date": "2020-01-02",
-        "item": "Mozzarella Sticks with Marinara Sauce"
-    },
-    {
-        "date": "2020-01-03",
-        "item": "Pizza Slice"
-    },
-    {
-        "date": "2020-01-06",
+        "date": "2020-02-03",
         "item": "Grilled Cheese Sandwich"
     },
     {
-        "date": "2020-01-07",
-        "item": "Chicken Fajita"
+        "date": "2020-02-04",
+        "item": "Turkey Nachos Grande"
     },
     {
-        "date": "2020-01-08",
-        "item": "Meatball Hero Parmesan"
-    },
-    {
-        "date": "2020-01-09",
-        "item": "Crispy Chicken Patty on a Bun"
-    },
-    {
-        "date": "2020-01-10",
-        "item": "Pizza Cheese Crunchers"
-    },
-    {
-        "date": "2020-01-13",
-        "item": "Maple Glazed French Toast Sticks"
-    },
-    {
-        "date": "2020-01-14",
-        "item": "Hot Dog on Bun"
-    },
-    {
-        "date": "2020-01-15",
+        "date": "2020-02-05",
         "item": "Pasta with Meat Sauce"
     },
     {
-        "date": "2020-01-16",
+        "date": "2020-02-06",
         "item": "Chicken Tenders"
     },
     {
-        "date": "2020-01-17",
-        "item": "Pizza Slice"
+        "date": "2020-02-07",
+        "item": "Pizza Cheese Crunchers"
     },
     {
-        "date": "2020-01-21",
-        "item": "Popcorn Chicken"
+        "date": "2020-02-10",
+        "item": "Maple Glazed French Toast Sticks"
     },
     {
-        "date": "2020-01-22",
-        "item": "Hamburger or Cheeseburger or Veggie Burger on a Bun"
-    },
-    {
-        "date": "2020-01-23",
-        "item": "Homemade Macaroni and Cheese"
-    },
-    {
-        "date": "2020-01-24",
-        "item": "Pizza Sticks"
-    },
-    {
-        "date": "2020-01-27",
+        "date": "2020-02-11",
         "item": "Spicy or Plain Chicken Patty on a Bun"
     },
     {
-        "date": "2020-01-28",
-        "item": "Twin Turkey Tacos"
+        "date": "2020-02-12",
+        "item": "Hamburger or Cheeseburger or Veggie Burger on a Bun"
     },
     {
-        "date": "2020-01-29",
-        "item": "Mozzarella Sticks with Marinara Sauce"
-    },
-    {
-        "date": "2020-01-30",
+        "date": "2020-02-13",
         "item": "Chicken Nuggets"
     },
     {
-        "date": "2020-01-31",
+        "date": "2020-02-14",
+        "item": "Pizza Sticks"
+    },
+    {
+        "date": "2020-02-24",
+        "item": "Sausage, Egg and Cheese on a Bun"
+    },
+    {
+        "date": "2020-02-25",
+        "item": "Hot Dog on Bun"
+    },
+    {
+        "date": "2020-02-26",
+        "item": "Mozzarella Sticks with Marinara Sauce"
+    },
+    {
+        "date": "2020-02-27",
+        "item": "Popcorn Chicken"
+    },
+    {
+        "date": "2020-02-28",
         "item": "Pizza Slice"
     }
 ]
