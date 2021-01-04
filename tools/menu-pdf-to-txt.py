3d6ca7729aca63490559e6f35bf61505924b1d46
nshenoy <nithin_shenoy@hotmail.com>
2021-01-04 09:17:22 -0500

    Updates for January 2021

---
+++ tools/menu-pdf-to-txt.py
@@ -0,0 +1,22 @@
+import sys
+import json
+from pdfminer.high_level import extract_text
+
+def main():
+    # read in arguments
+    menuFile = sys.argv[1]
+    txtFile = menuFile.replace(".pdf", ".txt")
+
+    # convert the menu PDF file to text
+    convertMenuToText(menuFile, txtFile)
+
+def convertMenuToText(menuFile, textFile):
+    # read in the contents of the menu txt file
+    menuRawText = extract_text(menuFile)
+
+    # save the text file
+    with open(textFile, "w") as fout:
+        fout.write(menuRawText)
+
+if __name__ == "__main__":
+    main()
