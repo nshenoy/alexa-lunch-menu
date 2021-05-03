7b26b4dca05586209dac20338f929a64a90c28de
nshenoy <nithin_shenoy@hotmail.com>
2021-05-02 23:51:51 -0400

    Adds May 2021 Menu

--- lambda/py/alexa/data.py
+++ lambda/py/alexa/data.py
@@ -11,160 +11,160 @@ ABOUT = _("I'll tell you what's for lunch today or tomorrow in Locust Valley Ele
 STOP = _("Okay, see you next time!")
 FALLBACK = _("The {} can't help you with that.")
 GENERIC_REPROMPT = _("What can I help you with?")
-LUNCHMENU = "April2021"
+LUNCHMENU = "May2021"
 LUNCHMENU_DATA_ELEMENTARY = [
     {
-        "date": "2021-04-06",
-        "item": "Mozzarella Sticks with Marinara Sauce"
-    },
-    {
-        "date": "2021-04-07",
-        "item": "Hamburger on a Bun"
-    },
-    {
-        "date": "2021-04-08",
-        "item": "Chicken Tenders"
-    },
-    {
-        "date": "2021-04-09",
-        "item": "Pizza Slice"
-    },
-    {
-        "date": "2021-04-12",
-        "item": "Grilled Cheese Sandwich"
-    },
-    {
-        "date": "2021-04-13",
-        "item": "Pizza Cheese Crunchers"
-    },
-    {
-        "date": "2021-04-14",
-        "item": "Hamburger on a Bun"
-    },
-    {
-        "date": "2021-04-15",
-        "item": "Chicken Nuggets"
-    },
-    {
-        "date": "2021-04-16",
-        "item": "Pizza Slice"
-    },
-    {
-        "date": "2021-04-19",
-        "item": "Grilled Cheese Sandwich"
-    },
-    {
-        "date": "2021-04-20",
-        "item": "Pizza Cheese Crunchers"
-    },
-    {
-        "date": "2021-04-21",
-        "item": "Hamburger on a Bun"
-    },
-    {
-        "date": "2021-04-22",
-        "item": "Chicken Nuggets"
-    },
-    {
-        "date": "2021-04-23",
-        "item": "Pizza Slice"
-    },
-    {
-        "date": "2021-04-26",
-        "item": "Grilled Cheese Sandwich"
-    },
-    {
-        "date": "2021-04-27",
+        "date": "2021-05-03",
         "item": "Maple Glazed French Toast Sticks"
     },
     {
-        "date": "2021-04-28",
-        "item": "Hamburger on a Bun"
+        "date": "2021-05-04",
+        "item": "Pizza Cheese Crunchers"
     },
     {
-        "date": "2021-04-29",
+        "date": "2021-05-05",
+        "item": "Cheeseburger on a Bun"
+    },
+    {
+        "date": "2021-05-06",
         "item": "Chicken Nuggets"
     },
     {
-        "date": "2021-04-30",
+        "date": "2021-05-07",
         "item": "Pizza Slice"
+    },
+    {
+        "date": "2021-05-10",
+        "item": "Grilled Cheese Sandwich"
+    },
+    {
+        "date": "2021-05-11",
+        "item": "Mozzarella Sticks with Marinara Sauce"
+    },
+    {
+        "date": "2021-05-12",
+        "item": "Cheeseburger on a Bun"
+    },
+    {
+        "date": "2021-05-13",
+        "item": "Popcorn Chicken"
+    },
+    {
+        "date": "2021-05-14",
+        "item": "Pizza Slice"
+    },
+    {
+        "date": "2021-05-17",
+        "item": "Maple Glazed French Toast Sticks"
+    },
+    {
+        "date": "2021-05-18",
+        "item": "Pizza Sticks"
+    },
+    {
+        "date": "2021-05-19",
+        "item": "Cheeseburger on a Bun"
+    },
+    {
+        "date": "2021-05-20",
+        "item": "Chicken Nuggets"
+    },
+    {
+        "date": "2021-05-21",
+        "item": "Pizza Slice"
+    },
+    {
+        "date": "2021-05-24",
+        "item": "Grilled Cheese Sandwich"
+    },
+    {
+        "date": "2021-05-25",
+        "item": "Pizza Cheese Crunchers"
+    },
+    {
+        "date": "2021-05-26",
+        "item": "Hot Dog on a Bun"
+    },
+    {
+        "date": "2021-05-27",
+        "item": "Chicken Nuggets"
     }
 ]
 LUNCHMENU_DATA_MIDDLESCHOOL = [
     {
-        "date": "2021-04-06",
-        "item": "A:Maple Glazed French Toast Sticks"
-    },
-    {
-        "date": "2021-04-07",
-        "item": "B:Hamburger, Cheeseburger or Veggie Burger on a Bun"
-    },
-    {
-        "date": "2021-04-08",
-        "item": "A:Chicken Tenders"
-    },
-    {
-        "date": "2021-04-09",
-        "item": "B:Pizza Sticks"
-    },
-    {
-        "date": "2021-04-12",
-        "item": "A:Egg and Cheese with Canadian Bacon on a Mini Bagel"
-    },
-    {
-        "date": "2021-04-13",
-        "item": "B:Pizza Cheese Crunchers"
-    },
-    {
-        "date": "2021-04-14",
-        "item": "A:Barbeque Beef Rib Sandwich"
-    },
-    {
-        "date": "2021-04-15",
-        "item": "B:Popcorn Chicken"
-    },
-    {
-        "date": "2021-04-16",
-        "item": "A:Pizza Slice"
-    },
-    {
-        "date": "2021-04-19",
-        "item": "B:Grilled Cheese Sandwich"
-    },
-    {
-        "date": "2021-04-20",
-        "item": "A:Turkey Tacos"
-    },
-    {
-        "date": "2021-04-21",
-        "item": "B:Meatball Hero Parmesan"
-    },
-    {
-        "date": "2021-04-22",
-        "item": "A:Baked Chicken"
-    },
-    {
-        "date": "2021-04-23",
-        "item": "B:Pizza Slice"
-    },
-    {
-        "date": "2021-04-26",
-        "item": "A:Mozzarella Sticks with Marinara Sauce"
-    },
-    {
-        "date": "2021-04-27",
+        "date": "2021-05-03",
         "item": "B:Maple Glazed French Toast Sticks"
     },
     {
-        "date": "2021-04-28",
+        "date": "2021-05-04",
+        "item": "A:Macaroni and Cheese"
+    },
+    {
+        "date": "2021-05-05",
+        "item": "B:Nachos"
+    },
+    {
+        "date": "2021-05-06",
+        "item": "A:Chicken Teriyaki"
+    },
+    {
+        "date": "2021-05-07",
+        "item": "B:Pizza Slice"
+    },
+    {
+        "date": "2021-05-10",
+        "item": "A:Mozzarella Sticks with Marinara Sauce"
+    },
+    {
+        "date": "2021-05-11",
+        "item": "B:Turkey Tacos"
+    },
+    {
+        "date": "2021-05-12",
         "item": "A:Hamburger, Cheeseburger or Veggie Burger on a Bun"
     },
     {
-        "date": "2021-04-29",
+        "date": "2021-05-13",
         "item": "B:Chicken Tenders"
     },
     {
-        "date": "2021-04-30",
+        "date": "2021-05-14",
         "item": "A:Pizza Slice"
+    },
+    {
+        "date": "2021-05-17",
+        "item": "B:Maple Glazed French Toast Sticks"
+    },
+    {
+        "date": "2021-05-18",
+        "item": "A:Pizza Sticks with Marinara Sauce"
+    },
+    {
+        "date": "2021-05-19",
+        "item": "B:Pasta with Meatballs"
+    },
+    {
+        "date": "2021-05-20",
+        "item": "A:Chicken Teriyaki"
+    },
+    {
+        "date": "2021-05-21",
+        "item": "B:Pizza Slice"
+    },
+    {
+        "date": "2021-05-24",
+        "item": "A:Mozzarella Sticks with Marinara Sauce"
+    },
+    {
+        "date": "2021-05-25",
+        "item": "B:Buffalo or Plain Pizza Cheese Crunchers"
+    },
+    {
+        "date": "2021-05-26",
+        "item": "A:Hamburger, Cheeseburger or Veggie Burger on a Bun"
+    },
+    {
+        "date": "2021-05-27",
+        "item": "B:Chicken Nuggets"
     }
 ]
