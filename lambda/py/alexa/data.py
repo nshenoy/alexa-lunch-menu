c83a7312e465cc1a45be37e4de3c785e5cbea0ee
nshenoy <nithin_shenoy@hotmail.com>
2021-11-30 16:28:33 -0500

    Adds November menu

--- lambda/py/alexa/data.py
+++ lambda/py/alexa/data.py
@@ -11,216 +11,136 @@ ABOUT = _("I'll tell you what's for lunch today or tomorrow in Locust Valley Ele
 STOP = _("Okay, see you next time!")
 FALLBACK = _("The {} can't help you with that.")
 GENERIC_REPROMPT = _("What can I help you with?")
-LUNCHMENU = "September2021"
+LUNCHMENU = "December2021"
 LUNCHMENU_DATA_ELEMENTARY = [
     {
-        "date": "2021-09-23",
-        "item": "Chicken nuggets"
+        "date": "2021-12-01",
+        "item": "Eggoji Waffles and Turkey Sausage Patty"
     },
     {
-        "date": "2021-09-24",
-        "item": "Pizza slice"
+        "date": "2021-12-02",
+        "item": "Chicken Nuggets"
     },
     {
-        "date": "2021-09-27",
-        "item": "Macaroni and cheese"
+        "date": "2021-12-03",
+        "item": "Cheese Pizza Slice"
     },
     {
-        "date": "2021-09-28",
-        "item": "Beef nachos with cheese"
+        "date": "2021-12-06",
+        "item": "Homemade Mac and Cheese"
     },
     {
-        "date": "2021-09-29",
-        "item": "Baked ziti with garlic bread"
+        "date": "2021-12-07",
+        "item": "Cheese Quesadilla"
     },
     {
-        "date": "2021-09-30",
-        "item": "Chicken nuggets"
+        "date": "2021-12-08",
+        "item": "Hamburger or Cheeseburger on a Bun"
     },
     {
-        "date": "2021-10-01",
-        "item": "Cheese pizza"
+        "date": "2021-12-09",
+        "item": "Cheese Pizza Slice"
     },
     {
-        "date": "2021-10-04",
-        "item": "Grilled cheese sandwich"
+        "date": "2021-12-10",
+        "item": "Half Day Dismissal- No Lunch Service"
     },
     {
-        "date": "2021-10-05",
-        "item": "Penne Chicken Alfredo with Bread Stick"
+        "date": "2021-12-13",
+        "item": "Grilled Cheese"
     },
     {
-        "date": "2021-10-06",
-        "item": "Beef Burger with or without Cheese"
+        "date": "2021-12-14",
+        "item": "Twin Soft Tacos and Shredded Cheddar"
     },
     {
-        "date": "2021-10-07",
-        "item": "Chicken Tenders"
+        "date": "2021-12-15",
+        "item": "Hamburger or Cheeseburger on a Bun"
     },
     {
-        "date": "2021-10-08",
-        "item": "Cheese pizza"
+        "date": "2021-12-16",
+        "item": "Chicken Nuggets"
     },
     {
-        "date": "2021-10-12",
-        "item": "Homemade Macaroni and Cheese"
+        "date": "2021-12-17",
+        "item": "Cheese Pizza Slice"
     },
     {
-        "date": "2021-10-13",
-        "item": "Mozzarella Sticks with Marinara Sauce"
+        "date": "2021-12-20",
+        "item": "Pizza Crunchers"
     },
     {
-        "date": "2021-10-14",
-        "item": "Popcorn Chicken"
+        "date": "2021-12-21",
+        "item": "Mozzarella Sticks"
     },
     {
-        "date": "2021-10-15",
-        "item": "Cheese pizza"
-    },
-    {
-        "date": "2021-10-18",
-        "item": "Egg and Cheese Sandwich on a Roll"
-    },
-    {
-        "date": "2021-10-19",
-        "item": "Cheese Quesadilla With Salsa and Sour Cream"
-    },
-    {
-        "date": "2021-10-20",
-        "item": "Pizza Cheese Crunchers"
-    },
-    {
-        "date": "2021-10-21",
-        "item": "Chicken tenders"
-    },
-    {
-        "date": "2021-10-22",
-        "item": "Cheese pizza"
-    },
-    {
-        "date": "2021-10-25",
-        "item": "Mini Maple Waffles with Sausage Patty"
-    },
-    {
-        "date": "2021-10-26",
-        "item": "Twin tacos"
-    },
-    {
-        "date": "2021-10-27",
-        "item": "Baked Ziti with Garlic Bread"
-    },
-    {
-        "date": "2021-10-28",
-        "item": "Chicken nuggets"
-    },
-    {
-        "date": "2021-10-29",
-        "item": "Cheese pizza"
+        "date": "2021-12-22",
+        "item": "Cheese Pizza Slice"
     }
 ]
 LUNCHMENU_DATA_MIDDLESCHOOL = [
     {
-        "date": "2021-09-23",
-        "item": "B:Chicken nuggets"
+        "date": "2021-12-01",
+        "item": "A:Pizza sticks"
     },
     {
-        "date": "2021-09-24",
-        "item": "A:Pizza slice"
+        "date": "2021-12-02",
+        "item": "B:Chicken Nuggets"
     },
     {
-        "date": "2021-09-27",
-        "item": "B:Macaroni and cheese"
+        "date": "2021-12-03",
+        "item": "A:Cheese Pizza Slice"
     },
     {
-        "date": "2021-09-28",
-        "item": "A:Beef nachos with cheese"
+        "date": "2021-12-06",
+        "item": "B:Homemade Mac and Cheese"
     },
     {
-        "date": "2021-09-29",
-        "item": "B:Baked ziti with garlic bread"
+        "date": "2021-12-07",
+        "item": "A:Oven roasted chicken"
     },
     {
-        "date": "2021-09-30",
+        "date": "2021-12-08",
+        "item": "B:Hamburger or Cheeseburger on a Bun"
+    },
+    {
+        "date": "2021-12-09",
         "item": "A:Chicken nuggets"
     },
     {
-        "date": "2021-10-01",
-        "item": "B:Cheese pizza"
+        "date": "2021-12-10",
+        "item": "B:Cheese Pizza Slice"
     },
     {
-        "date": "2021-10-04",
-        "item": "A:Grilled cheese sandwich"
+        "date": "2021-12-13",
+        "item": "A:Grilled Cheese"
     },
     {
-        "date": "2021-10-05",
-        "item": "B:Roasted Chicken and Mashed Potatoes"
+        "date": "2021-12-14",
+        "item": "B:Twin Soft Tacos and Shredded Cheddar"
     },
     {
-        "date": "2021-10-06",
-        "item": "A:Beef Burger with or without Cheese"
+        "date": "2021-12-15",
+        "item": "A:Hamburger or Cheeseburger on a Bun"
     },
     {
-        "date": "2021-10-07",
-        "item": "B:Chicken Tenders"
+        "date": "2021-12-16",
+        "item": "B:Chicken Nuggets"
     },
     {
-        "date": "2021-10-08",
-        "item": "A:Cheese pizza"
+        "date": "2021-12-17",
+        "item": "A:Cheese Pizza Slice"
     },
     {
-        "date": "2021-10-12",
-        "item": "B:Homemade Macaroni and Cheese"
+        "date": "2021-12-20",
+        "item": "B:Grilled cheese"
     },
     {
-        "date": "2021-10-13",
-        "item": "A:Smokey BBQ Chicken Panini with Cheddar Cheese"
+        "date": "2021-12-21",
+        "item": "A:Mozzarella Sticks"
     },
     {
-        "date": "2021-10-14",
-        "item": "B:Popcorn Chicken"
-    },
-    {
-        "date": "2021-10-15",
-        "item": "A:Cheese pizza"
-    },
-    {
-        "date": "2021-10-18",
-        "item": "B:Egg and Cheese Sandwich on a Roll"
-    },
-    {
-        "date": "2021-10-19",
-        "item": "A:Chicken Fajitas"
-    },
-    {
-        "date": "2021-10-20",
-        "item": "B:Mozzarella Sticks with Marinara Sauce"
-    },
-    {
-        "date": "2021-10-21",
-        "item": "A:Chicken tenders"
-    },
-    {
-        "date": "2021-10-22",
-        "item": "B:Cheese pizza"
-    },
-    {
-        "date": "2021-10-25",
-        "item": "A:Crispy Chicken Patty Sandwich"
-    },
-    {
-        "date": "2021-10-26",
-        "item": "B:Nachos Grande"
-    },
-    {
-        "date": "2021-10-27",
-        "item": "A:Grilled Chicken Panini with Mozzarella and Tomato"
-    },
-    {
-        "date": "2021-10-28",
-        "item": "B:Chicken nuggets"
-    },
-    {
-        "date": "2021-10-29",
-        "item": "A:Cheese pizza"
+        "date": "2021-12-22",
+        "item": "B:Cheese Pizza Slice"
     }
 ]
