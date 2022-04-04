992088801dc002bc4183762c0e2347b1ee00a1f7
nshenoy <nithin_shenoy@hotmail.com>
2022-04-04 07:23:06 -0400

    Adds April menu

--- lambda/py/alexa/data.py
+++ lambda/py/alexa/data.py
@@ -11,192 +11,128 @@ ABOUT = _("I'll tell you what's for lunch today or tomorrow in Locust Valley Ele
 STOP = _("Okay, see you next time!")
 FALLBACK = _("The {} can't help you with that.")
 GENERIC_REPROMPT = _("What can I help you with?")
-LUNCHMENU = "March2022"
+LUNCHMENU = "April2022"
 LUNCHMENU_DATA_ELEMENTARY = [
     {
-        "date": "2022-03-01",
-        "item": "Pasta with Meatballs"
-    },
-    {
-        "date": "2022-03-02",
-        "item": "Eggoji Waffles"
-    },
-    {
-        "date": "2022-03-03",
-        "item": "Chicken Nuggets"
-    },
-    {
-        "date": "2022-03-04",
+        "date": "2022-04-01",
         "item": "Cheese Pizza Slice"
     },
     {
-        "date": "2022-03-07",
-        "item": "Macaroni & Cheese"
+        "date": "2022-04-04",
+        "item": "Macaroni and Cheese"
     },
     {
-        "date": "2022-03-08",
+        "date": "2022-04-05",
+        "item": "Eggoji Waffles"
+    },
+    {
+        "date": "2022-04-06",
         "item": "All Beef Hot Dog on a Bun"
     },
     {
-        "date": "2022-03-09",
-        "item": "Eggo Mini Pancakes"
-    },
-    {
-        "date": "2022-03-10",
+        "date": "2022-04-07",
         "item": "Chicken Nuggets"
     },
     {
-        "date": "2022-03-11",
+        "date": "2022-04-08",
         "item": "Cheese Pizza Slice"
     },
     {
-        "date": "2022-03-14",
+        "date": "2022-04-11",
         "item": "Grilled Cheese"
     },
     {
-        "date": "2022-03-15",
-        "item": "Beef Tacos"
-    },
-    {
-        "date": "2022-03-16",
-        "item": "Eggoji Waffles"
-    },
-    {
-        "date": "2022-03-17",
-        "item": "Chicken Nuggets"
-    },
-    {
-        "date": "2022-03-18",
-        "item": "Cheese Pizza Slice"
-    },
-    {
-        "date": "2022-03-21",
-        "item": "Grilled Cheese"
-    },
-    {
-        "date": "2022-03-22",
+        "date": "2022-04-12",
         "item": "Pizza Crunchers"
     },
     {
-        "date": "2022-03-23",
-        "item": "French Toast Sticks"
-    },
-    {
-        "date": "2022-03-24",
-        "item": "Cheeseburger"
-    },
-    {
-        "date": "2022-03-25",
-        "item": "No lunch because it's a half day"
-    },
-    {
-        "date": "2022-03-28",
-        "item": "Grilled Cheese"
-    },
-    {
-        "date": "2022-03-29",
-        "item": "Pizza Sticks"
-    },
-    {
-        "date": "2022-03-30",
+        "date": "2022-04-13",
         "item": "Pasta with Meatballs"
     },
     {
-        "date": "2022-03-31",
+        "date": "2022-04-14",
+        "item": "Cheese Pizza Slice"
+    },
+    {
+        "date": "2022-04-25",
+        "item": "Grilled Cheese"
+    },
+    {
+        "date": "2022-04-26",
+        "item": "Pizza Sticks"
+    },
+    {
+        "date": "2022-04-27",
+        "item": "Penne Chicken Alfredo"
+    },
+    {
+        "date": "2022-04-28",
         "item": "Chicken Nuggets"
+    },
+    {
+        "date": "2022-04-29",
+        "item": "Cheese Pizza Slice"
     }
 ]
 LUNCHMENU_DATA_MIDDLESCHOOL = [
     {
-        "date": "2022-03-01",
-        "item": "A:Chili con Carne"
-    },
-    {
-        "date": "2022-03-02",
-        "item": "B:Baked Ziti"
-    },
-    {
-        "date": "2022-03-03",
-        "item": "A:Chicken Nuggets"
-    },
-    {
-        "date": "2022-03-04",
+        "date": "2022-04-01",
         "item": "B:Cheese Pizza Slice"
     },
     {
-        "date": "2022-03-07",
-        "item": "A:Pizza Sticks"
-    },
-    {
-        "date": "2022-03-08",
-        "item": "B:Chicken Quesadilla"
-    },
-    {
-        "date": "2022-03-09",
-        "item": "A:All Beef Hot Dog on a Bun"
-    },
-    {
-        "date": "2022-03-10",
-        "item": "B:Chicken Nuggets"
-    },
-    {
-        "date": "2022-03-11",
-        "item": "A:Cheese Pizza Slice"
-    },
-    {
-        "date": "2022-03-14",
-        "item": "B:Grilled Cheese"
-    },
-    {
-        "date": "2022-03-15",
-        "item": "A:Beef Tacos"
-    },
-    {
-        "date": "2022-03-16",
-        "item": "B:Cheeseburger"
-    },
-    {
-        "date": "2022-03-17",
+        "date": "2022-04-04",
         "item": "A:Chicken Nuggets"
     },
     {
-        "date": "2022-03-18",
-        "item": "B:Cheese Pizza Slice"
-    },
-    {
-        "date": "2022-03-21",
-        "item": "A:Grilled Cheese"
-    },
-    {
-        "date": "2022-03-22",
+        "date": "2022-04-05",
         "item": "B:French Toast Sticks"
     },
     {
-        "date": "2022-03-23",
-        "item": "A:Cheeseburger"
+        "date": "2022-04-06",
+        "item": "A:Meatball Hero"
     },
     {
-        "date": "2022-03-24",
+        "date": "2022-04-07",
         "item": "B:Chicken Nuggets"
     },
     {
-        "date": "2022-03-25",
+        "date": "2022-04-08",
         "item": "A:Cheese Pizza Slice"
     },
     {
-        "date": "2022-03-28",
+        "date": "2022-04-11",
         "item": "B:Grilled Cheese"
     },
     {
-        "date": "2022-03-29",
-        "item": "A:Pizza Sticks"
+        "date": "2022-04-12",
+        "item": "A:Oven Roasted Chicken"
     },
     {
-        "date": "2022-03-30",
-        "item": "B:Pizza Crunchers"
+        "date": "2022-04-13",
+        "item": "B:Pasta with Meat Sauce"
     },
     {
-        "date": "2022-03-31",
-        "item": "A:Chicken Nuggets"
+        "date": "2022-04-14",
+        "item": "A:Cheese Pizza Slice"
+    },
+    {
+        "date": "2022-04-25",
+        "item": "B:French Toast Sticks"
+    },
+    {
+        "date": "2022-04-26",
+        "item": "B:Mozarella Sticks"
+    },
+    {
+        "date": "2022-04-27",
+        "item": "B:Turkey Nachos Grande"
+    },
+    {
+        "date": "2022-04-28",
+        "item": "A:Chicken Caesar Wrap"
+    },
+    {
+        "date": "2022-04-29",
+        "item": "B:Cheese Pizza Slice"
     }
 ]
